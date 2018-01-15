f97062dc00c29afdbf39f93c7f12d15c0df4ecc9
Guster <guster@K025-GusterChua.local>
2018-01-15 13:40:22 +0800

    added circleci config

--- app/src/main/java/com/gusterwoei/calabashtest/LoginActivity.java
+++ app/src/main/java/com/gusterwoei/calabashtest/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ViewUtils;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -45,9 +47,13 @@ public class LoginActivity extends BaseActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                finish();
+                proceed();
             }
         }, 3000);
+    }
+
+    private void proceed() {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        //finish();
     }
 }
--- app/src/main/java/com/gusterwoei/calabashtest/MainActivity.java
+++ app/src/main/java/com/gusterwoei/calabashtest/MainActivity.java
@@ -3,6 +3,8 @@ package com.gusterwoei.calabashtest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import butterknife.OnClick;
 
@@ -14,7 +16,23 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @OnClick(R.id.btn_logout)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                logout();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //@OnClick(R.id.btn_logout)
     protected void logout() {
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
