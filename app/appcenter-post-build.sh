082e5c8c755a02c74e212b001bb1acb32daa4d5a
Guster <guster@K025-GusterChua.local>
2018-02-08 17:22:44 +0800

    Integration with GooglePlay Developer API to enable automated app publish

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -37,4 +37,6 @@ if [[ $branch =~ ^"develop-v".*$ || $branch =~ ^"develop"$ ]]; then
    fi
 elif [ $branch == "master" ]; then
    # deploy to google playstore
+   # NOTE: Remember to update the 'whatsnew' file for the release note
+   ./gradlew publishApkProdRelease
 fi
